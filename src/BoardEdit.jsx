23ad7f7b2a7e776c83debdeabab9afbe6bb38ce7
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-21 16:44:43 +0900

    수정 후 토스트/이동, 수정 확인 모달

--- src/BoardEdit.jsx
+++ src/BoardEdit.jsx
@@ -4,22 +4,55 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function BoardEdit() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const navigate = useNavigate();
+  const toast = useToast();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   useEffect(() => {
     axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
 
   function handleClickSave() {
-    axios.put(`/api/board/edit`, board);
+    axios
+      .put(`/api/board/edit`, board)
+      .then(
+        toast({
+          status: "success",
+          description: "수정완료하였습니다",
+          position: "top",
+        }),
+        navigate(`/board/${id}`),
+      )
+      .catch((error) => {
+        if (error.response.status === 400) {
+          toast({
+            status: "error",
+            description: "수정 실패하였습니다",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => {
+        onClose();
+      });
   }
 
   if (board === null) {
@@ -58,10 +91,23 @@ export function BoardEdit() {
         </FormControl>
       </Box>
       <Box>
-        <Button onClick={handleClickSave} colorScheme={"blue"}>
+        <Button colorScheme={"blue"} onClick={onOpen}>
           저장
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalBody>저장하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button onClick={handleClickSave} colorScheme={"blue"}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
