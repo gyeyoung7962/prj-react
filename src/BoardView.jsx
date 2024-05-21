a2aa7567884ad285ef51178504c4da084045f4b0
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-21 15:40:46 +0900

    삭제시 모달로 확인

--- src/BoardView.jsx
+++ src/BoardView.jsx
@@ -7,8 +7,15 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 
@@ -17,6 +24,8 @@ export function BoardView() {
   const [board, setBoard] = useState(null);
   const toast = useToast();
   const navigate = useNavigate();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   useEffect(() => {
     axios
       .get(`/api/board/${id}`)
@@ -80,10 +89,23 @@ export function BoardView() {
       </Box>
       <Box>
         <Button colorScheme={"purple"}>수정</Button>
-        <Button colorScheme={"red"} onClick={handleClickRemove}>
+        <Button colorScheme={"red"} onClick={onOpen}>
           삭제
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button colorScheme={"red"} onClick={handleClickRemove}>
+              확인
+            </Button>
+            <Button onClick={onClose}>취소</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
