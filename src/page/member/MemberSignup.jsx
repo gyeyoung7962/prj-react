dc7a3c02565460951bf3d5acc7f696866f46a2f0
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-22 10:32:04 +0900

    이메일 사용 가능한 지 확인 404 -> 없으니까 사용 가능 200 -> 있으니까 사용 불가능

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -4,6 +4,8 @@ import {
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
@@ -50,13 +52,43 @@ export function MemberSignup() {
       .finally(setIsLoading(false));
   }
 
+  function handleCheckEmail() {
+    axios
+      .get(`/api/member/check?email=${email}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 이메일입니다",
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          //사용할 수 있는 이메일
+          toast({
+            status: "info",
+            description: "사용할 수 있는 이메일입니다",
+            position: "top",
+          });
+        }
+      })
+      .finally();
+  }
+
   return (
     <Box>
       <Box>회원가입</Box>
       <Box>
         <FormControl>
           <FormLabel>이메일</FormLabel>
-          <Input onChange={(e) => setEmail(e.target.value)} />
+          <InputGroup>
+            <Input onChange={(e) => setEmail(e.target.value)} />
+            <InputRightElement w={"75px"} mr={1}>
+              <Button onClick={handleCheckEmail} size={"sm"}>
+                중복확인
+              </Button>
+            </InputRightElement>
+          </InputGroup>
         </FormControl>
         <Box>
           <FormControl>
