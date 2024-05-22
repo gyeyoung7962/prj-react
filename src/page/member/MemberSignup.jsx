3987ebdf01de70ed27700e45fab46b3247ee7e23
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-22 09:33:04 +0900

    회원가입 화면 가입 버튼 클릭시 post /api/member/signup 전송 then, catch, finally 작성

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -1,28 +1,83 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function MemberSignup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    axios
+      .post("/api/member/signup", {
+        email,
+        password,
+        nickName,
+      })
+      .then((res) => {
+        toast({
+          status: "success",
+          description: "회원가입 성공",
+          position: "top",
+        }),
+          navigate("/");
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          toast({
+            status: "error",
+            description: "입력값을 확인해주세요",
+            position: "top",
+          });
+        }
+        toast({
+          status: "error",
+          description: "회원가입 실패",
+          position: "top",
+        });
+      })
+      .finally(setIsLoading(false));
+  }
+
   return (
     <Box>
       <Box>회원가입</Box>
       <Box>
         <FormControl>
           <FormLabel>이메일</FormLabel>
-          <Input />
+          <Input onChange={(e) => setEmail(e.target.value)} />
         </FormControl>
         <Box>
           <FormControl>
             <FormLabel>암호</FormLabel>
-            <Input />
+            <Input onChange={(e) => setPassword(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
           <FormControl>
             <FormLabel>별명</FormLabel>
-            <Input />
+            <Input onChange={(e) => setNickName(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"}>가입</Button>
+          <Button
+            isLoading={isLoading}
+            colorScheme={"blue"}
+            onClick={handleClick}
+          >
+            가입
+          </Button>
         </Box>
       </Box>
     </Box>
