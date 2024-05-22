4bf3e0abbebbf18f7d9cb4fa89b9a743f080569c
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-22 11:08:48 +0900

    별명 사용 가능한 지 확인 404 -> 없으니까 사용 가능 200 -> 있으니까 사용 불가능

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -75,6 +75,29 @@ export function MemberSignup() {
       .finally();
   }
 
+  function handleCheckNickName() {
+    axios
+      .get(`/api/member/check?nickName=${nickName}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 별명 입니다",
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          //사용할 수 있는 별명
+          toast({
+            status: "info",
+            description: "사용할 수 있는 별명 입니다",
+            position: "top",
+          });
+        }
+      })
+      .finally();
+  }
+
   return (
     <Box>
       <Box>회원가입</Box>
@@ -99,7 +122,14 @@ export function MemberSignup() {
         <Box>
           <FormControl>
             <FormLabel>별명</FormLabel>
-            <Input onChange={(e) => setNickName(e.target.value)} />
+            <InputGroup>
+              <Input onChange={(e) => setNickName(e.target.value)} />
+              <InputRightElement w={"75px"} mr={1}>
+                <Button size={"sm"} onClick={handleCheckNickName}>
+                  중복확인
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
         </Box>
         <Box>
