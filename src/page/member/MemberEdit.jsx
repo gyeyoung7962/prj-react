9b79c43eb082f9dcb8cfc66c400aac76dbcb2315
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-23 11:20:23 +0900

    회원정보 변경시 기존 암호 확인

--- src/page/member/MemberEdit.jsx
+++ src/page/member/MemberEdit.jsx
@@ -5,20 +5,28 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
-import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function MemberEdit() {
-  const { id } = useParams();
   const [member, setMember] = useState(null);
-  const navigate = useNavigate();
+  const [oldPassword, setOldPassword] = useState("");
+  const { id } = useParams();
   const toast = useToast();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   useEffect(() => {
     axios
       .get(`/api/member/${id}`)
@@ -29,23 +37,22 @@ export function MemberEdit() {
       .catch(() => {
         toast({
           status: "warning",
-          description: "회원 정보 조회 중 문제가 발생하였습니다",
+          description: "회원 정보 조회 중 문제가 발생하였습니다.",
           position: "top",
         });
         navigate("/");
-      })
-      .finally();
+      });
   }, []);
 
   function handleClickSave() {
     axios
-      .put(`/api/member/modify`, member)
+      .put("/api/member/modify", { ...member, oldPassword })
       .then((res) => {})
-      .catch()
-      .finally();
+      .catch(() => {})
+      .finally(() => {});
   }
 
-  if (member == null) {
+  if (member === null) {
     return <Spinner />;
   }
 
@@ -53,46 +60,63 @@ export function MemberEdit() {
     <Box>
       <Box>회원 정보 수정</Box>
       <Box>
-        <FormControl>
-          <FormLabel>이메일</FormLabel>
-          <Input readOnly value={member.email} />
-        </FormControl>
-      </Box>
-
-      <Box>
-        <FormControl>
-          <FormLabel>암호</FormLabel>
+        <Box>
+          <FormControl>
+            <FormLabel>이메일</FormLabel>
+            <Input readOnly value={member.email} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>암호</FormLabel>
+            <Input
+              onChange={(e) =>
+                setMember({ ...member, password: e.target.value })
+              }
+              placeholder={"암호를 변경하려면 입력하세요"}
+            />
+            <FormHelperText>
+              입력하지 않으면 기존 암호를 변경하지 않습니다.
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>암호 확인</FormLabel>
+            <Input />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>별명</FormControl>
           <Input
-            placeholder={"암호를 변경하려면 입력하세요"}
-            onChange={(e) => setMember({ ...member, password: e.target.value })}
-          />
-          <FormHelperText>
-            입력하지 않으면 기존 암호로 변경하지 않습니다
-          </FormHelperText>
-        </FormControl>
-      </Box>
-
-      <Box>
-        <FormControl>
-          <FormLabel>암호 확인</FormLabel>
-          <Input />
-        </FormControl>
-      </Box>
-
-      <Box>
-        <FormControl>
-          <FormLabel>별명</FormLabel>
-          <Input
-            value={member.nickName}
             onChange={(e) => setMember({ ...member, nickName: e.target.value })}
+            value={member.nickName}
           />
-        </FormControl>
+        </Box>
+        <Box>
+          <Button onClick={onOpen} colorScheme={"blue"}>
+            저장
+          </Button>
+        </Box>
       </Box>
-      <Box>
-        <Button colorScheme={"blue"} onClick={handleClickSave}>
-          저장
-        </Button>
-      </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>기존 암호 확인</ModalHeader>
+          <ModalBody>
+            <FormControl>
+              <FormLabel>기존 암호</FormLabel>
+              <Input onChange={(e) => setOldPassword(e.target.value)} />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button colorScheme="blue" onClick={handleClickSave}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
