fca46885287d2dc05d5de818268e84374f6d8040
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-28 15:39:33 +0900

    추가 파일 선택하기

--- src/page/board/BoardEdit.jsx
+++ src/page/board/BoardEdit.jsx
@@ -1,8 +1,12 @@
+import { useNavigate, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
   Box,
   Button,
   Flex,
   FormControl,
+  FormHelperText,
   FormLabel,
   Image,
   Input,
@@ -19,48 +23,43 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
-import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 export function BoardEdit() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
   const [removeFileList, setRemoveFileList] = useState([]);
-  const navigate = useNavigate();
+  const [addFileList, setAddFileList] = useState([]);
   const toast = useToast();
+  const navigate = useNavigate();
   const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
     axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
 
-  console.log(removeFileList);
-
   function handleClickSave() {
     axios
-      .putForm(`/api/board/edit`, {
+      .putForm("/api/board/edit", {
         id: board.id,
         title: board.title,
         content: board.content,
         removeFileList,
       })
-      .then(
-        () =>
-          toast({
-            status: "success",
-            description: "수정완료하였습니다",
-            position: "top",
-          }),
-        navigate(`/board/${id}`),
-      )
-      .catch((error) => {
-        if (error.response.status === 400) {
+      .then(() => {
+        toast({
+          status: "success",
+          description: `${board.id}번 게시물이 수정되었습니다.`,
+          position: "top",
+        });
+        navigate(`/board/${board.id}`);
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
           toast({
             status: "error",
-            description: "수정 실패하였습니다",
+            description: `게시물이 수정되지 않았습니다. 작성한 내용을 확인해주세요.`,
             position: "top",
           });
         }
@@ -74,6 +73,11 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  const fileNameList = [];
+  for (let addFile of addFileList) {
+    fileNameList.push(<li>{addFile.name}</li>);
+  }
+
   function handleRemoveSwitchChange(name, checked) {
     if (checked) {
       setRemoveFileList([...removeFileList, name]);
@@ -84,69 +88,80 @@ export function BoardEdit() {
 
   return (
     <Box>
-      <Box>{id}번 게시물 수정</Box>
+      <Box>{board.id}번 게시물 수정</Box>
       <Box>
-        <FormControl>
-          <FormLabel>제목</FormLabel>
-          <Input
-            defaultValue={board.title}
-            onChange={(e) => setBoard({ ...board, title: e.target.value })}
-          />
-        </FormControl>
-      </Box>
-
-      <Box>
-        <FormControl>
-          <FormLabel>내용</FormLabel>
-          <Textarea
-            defaultValue={board.content}
-            onChange={(e) => setBoard({ ...board, content: e.target.value })}
-          />
-        </FormControl>
-      </Box>
-
-      <Box>
-        {board.fileList &&
-          board.fileList.map((file) => (
-            <Box border={"2px solid black"} m={3} key={file.name}>
-              <Flex>
-                <FontAwesomeIcon icon={faTrashCan} />
-                <Switch
-                  onChange={(e) =>
-                    handleRemoveSwitchChange(file.name, e.target.checked)
-                  }
-                />
-                <Text>{file.name}</Text>
-              </Flex>
-
-              <Box>
-                <Image
-                  sx={
-                    removeFileList.includes(file.name)
-                      ? { filter: "blur(8px)" }
-                      : {}
-                  }
-                  src={file.src}
-                />
+        <Box>
+          <FormControl>
+            <FormLabel>제목</FormLabel>
+            <Input
+              defaultValue={board.title}
+              onChange={(e) => setBoard({ ...board, title: e.target.value })}
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Textarea
+              defaultValue={board.content}
+              onChange={(e) => setBoard({ ...board, content: e.target.value })}
+            ></Textarea>
+          </FormControl>
+        </Box>
+        <Box>
+          {board.fileList &&
+            board.fileList.map((file) => (
+              <Box border={"2px solid black"} m={3} key={file.name}>
+                <Flex>
+                  <FontAwesomeIcon icon={faTrashCan} />
+                  <Switch
+                    onChange={(e) =>
+                      handleRemoveSwitchChange(file.name, e.target.checked)
+                    }
+                  />
+                  <Text>{file.name}</Text>
+                </Flex>
+                <Box>
+                  <Image
+                    sx={
+                      removeFileList.includes(file.name)
+                        ? { filter: "blur(8px)" }
+                        : {}
+                    }
+                    src={file.src}
+                  />
+                </Box>
               </Box>
-
-              {/*<Box>*/}
-              {/*  <Image src={file.src} />*/}
-              {/*</Box>*/}
-            </Box>
-          ))}
-      </Box>
-
-      <Box>
-        <FormControl>
-          <FormLabel>작성자</FormLabel>
-          <Input defaultValue={board.writer} readOnly={true} />
-        </FormControl>
-      </Box>
-      <Box>
-        <Button colorScheme={"blue"} onClick={onOpen}>
-          저장
-        </Button>
+            ))}
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>파일</FormLabel>
+            <Input
+              multiple
+              type="file"
+              accept="image/*"
+              onChange={(e) => setAddFileList(e.target.files)}
+            />
+            <FormHelperText>
+              총 용량은 10MB, 한 파일은 1MB를 초과할 수 없습니다.
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box>
+          <ul>{fileNameList}</ul>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input defaultValue={board.writer} readOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button colorScheme={"blue"} onClick={onOpen}>
+            저장
+          </Button>
+        </Box>
       </Box>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
