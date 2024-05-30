8bc9f83a1a9c2d272d8362f705873a558c1e45e7
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-30 11:17:38 +0900

    댓글 목록

--- src/component/comment/CommentList.jsx
+++ src/component/comment/CommentList.jsx
@@ -1,3 +1,32 @@
-export function CommentList() {
-  return null;
+import { useEffect } from "react";
+import axios from "axios";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
+
+export function CommentList({ boardId }) {
+  useEffect(() => {
+    axios
+      .get(`/api/comment/list/${boardId}`)
+      .then((res) => {
+        setCommentList(res.data);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {});
+  }, []);
+  if (commentList.length === 0) {
+    return <Box>댓글이 없습니다. 첫 댓글을 작성해보세요</Box>;
+  }
+  return (
+    <Box>
+      {commentList.map((comment) => (
+        <Box key={comment.id} border={"1px solid black"} my={3}>
+          <Flex>
+            <Box>{comment.memberId}</Box>
+            <Spacer />
+            <Box>{comment.regDate}</Box>
+            <Box>{comment.comment}</Box>
+          </Flex>
+        </Box>
+      ))}
+    </Box>
+  );
 }
