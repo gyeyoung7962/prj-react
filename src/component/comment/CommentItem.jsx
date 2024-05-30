c6077f4c4450b40f1692730056cd481dcc699c64
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-30 17:08:30 +0900

    댓글 삭제 시 모달로 확인, 삭제 후 토스트

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -1,9 +1,25 @@
-import { Alert, AlertIcon, Box, Button, Flex } from "@chakra-ui/react";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const toast = useToast();
   function handleRemoveClick() {
     setIsProcessing(true);
     axios
@@ -14,6 +30,13 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
       .catch((err) => {})
       .finally(() => {
         setIsProcessing(false);
+        onClose();
+        setIsProcessing(false);
+        toast({
+          description: "댓글이 삭제되었습니다.",
+          status: "info",
+          position: "top",
+        });
       });
   }
 
@@ -29,15 +52,29 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
         </Box>
       </Flex>
       <Flex>
-        <Button
-          isLoading={isProcessing}
-          colorScheme={"red"}
-          variant={"solid"}
-          onClick={() => handleRemoveClick()}
-        >
-          <FontAwesomeIcon icon={faTrash} />
-        </Button>
+        <Box>
+          <Button isLoading={isProcessing} colorScheme="red" onClick={onOpen}>
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+        </Box>
       </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalBody>댓글을 삭제 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button
+              isLoading={isProcessing}
+              colorScheme={"red"}
+              onClick={handleRemoveClick}
+            >
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
